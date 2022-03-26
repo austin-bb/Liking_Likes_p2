e27c7106d5f2d710701cae9b6e9588f31d45a96a
Austinbb <austinbaumbach2@gmail.com>
2022-03-26 15:52:11 -0700

    Liking_likes_p2

---
+++ script.js
@@ -0,0 +1,30 @@
+let likes1 = 9;
+let likes2 = 12;
+let likes3 = 9;
+
+function addLikes() {
+    likes1++;
+    let countElement = document.querySelector("#likes1");
+    countElement.innerText = likes1 + " like(s)";
+    console.log(likes1);
+}
+
+function addLikes2() {
+    likes2++;
+    let countElement = document.querySelector("#likes2");
+    countElement.innerText = likes2 + " like(s)";
+    console.log(likes2);
+}
+
+function addLikes3() {
+    likes3++;
+    let countElement = document.querySelector("#likes3");
+    countElement.innerText = likes3 + " like(s)";
+    console.log(likes3);
+}
+
+
+
+
+
+
